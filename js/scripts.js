bed9cf981157444215c19b5d8a4f03a3e8e6ac5e
Connor Cummings and John Klein <connor@>
2016-03-24 15:26:57 -0700

    fully functioning validation form

--- js/scripts.js
+++ js/scripts.js
@@ -13,26 +13,34 @@ var wednesday = function(){
 var addition = function(){
   var input = $("#q2-ans").val().toUpperCase();
   if(input === "YES"){
-    $("#q2").removeClass("has-error" && "has-warning").addClass("has-success");
+    $("#q2").removeClass("has-error has-warning").addClass("has-success");
   }else if(input === "NO"){
-    $("#q2").removeClass("has-success" && "has-warning").addClass("has-error");
+    $("#q2").removeClass("has-success has-warning").addClass("has-error");
   }else{
-    $("#q2").removeClass("has-error" && "has-success").addClass("has-warning");
+    $("#q2").removeClass("has-error has-success").addClass("has-warning");
   }
 
 }
+var coding = function(){
+  var input = $("input:radio[name=coding]:checked").val();
+  console.log(input);
+  if(input === "1"){
+    $("#q3").removeClass("has-error has-warning").addClass("has-success");
+  }else if(input === "0"){
+    $("#q3").removeClass("has-success has-warning").addClass("has-error");
+  }else{
+    $("#q3").removeClass("has-success has-error").addClass("has-warning");
+  }
 
 
 
-
-
-
-
+}
 $(document).ready(function(){
   $(".btn").click(function(){
     console.log("function working");
     wednesday();
     console.log("made it");
     addition();
+    coding();
   });
 });
